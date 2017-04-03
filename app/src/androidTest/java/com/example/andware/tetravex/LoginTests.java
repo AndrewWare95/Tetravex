8e877a415973d987ff5f68ea628058ac6ff2c71a
AndrewWare95 <Caerdydd2k16>
2017-04-03 17:21:26 +0100

    Added Espresso Tests

---
+++ app/src/androidTest/java/com/example/andware/tetravex/LoginTests.java
@@ -0,0 +1,92 @@
+package com.example.andware.tetravex;
+
+import android.support.design.widget.TextInputLayout;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class LoginTests {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(
+            LoginActivity.class);
+
+    @Before
+    public void setup(){
+    }
+
+    @Test
+    public void signIn(){
+        onView(withId(R.id.email)).check(matches(isDisplayed()));
+        onView(withId(R.id.email)).perform(typeText("Andrew"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withId(R.id.classicButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void signInPassword(){
+        onView(withId(R.id.email)).check(matches(isDisplayed()));
+        onView(withId(R.id.email)).perform(typeText("Andrew"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withId(R.id.classicButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void signInPasswordNonAlpha(){
+        onView(withId(R.id.email)).check(matches(isDisplayed()));
+        onView(withId(R.id.email)).perform(typeText("Andrew95"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withId(R.id.email)).check(matches(hasErrorText("Alphabetical characters only")));
+    }
+
+    @Test
+    public void signInPasswordTooLong(){
+        onView(withId(R.id.email)).check(matches(isDisplayed()));
+        onView(withId(R.id.email)).perform(typeText("AndrewWare"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withId(R.id.email)).check(matches(hasErrorText("Username maximum 10 characters")));
+
+    }
+
+    @Test
+    public void signInPasswordTooShort(){
+        onView(withId(R.id.email)).check(matches(isDisplayed()));
+        onView(withId(R.id.email)).perform(typeText("AW"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withId(R.id.email)).check(matches(hasErrorText("Username minimum 3 characters")));
+    }
+
+    @Test
+    public void signInNoPassword(){
+        onView(withId(R.id.email)).check(matches(isDisplayed()));
+        onView(withId(R.id.email)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withId(R.id.email_sign_in_button)).check(matches(isDisplayed()));
+    }
+
+    @After
+    public void finish(){
+
+    }
+}
---
+++ app/src/androidTest/java/com/example/andware/tetravex/MainMenuTests.java
@@ -0,0 +1,100 @@
+package com.example.andware.tetravex;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withTagKey;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainMenuTests {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(
+            LoginActivity.class);
+
+    @Before
+    public void setup(){
+    }
+
+    @Test
+    public void checkAllButtons(){
+        onView(withId(R.id.email)).check(matches(isDisplayed()));
+        onView(withId(R.id.email)).perform(typeText("Andrew"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withId(R.id.classicButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.timeTrialButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.unknownTitleButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.settingsButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.leaderboardButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.exitMainButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkButtonClassic(){
+        onView(withId(R.id.email)).check(matches(isDisplayed()));
+        onView(withId(R.id.email)).perform(typeText("Andrew"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withId(R.id.classicButton)).perform(click());
+        onView(withId(R.id.pause)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkButtonTimeTrial(){
+        onView(withId(R.id.email)).check(matches(isDisplayed()));
+        onView(withId(R.id.email)).perform(typeText("Andrew"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withId(R.id.timeTrialButton)).perform(click());
+        onView(withId(R.id.resetButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkButtonArcade(){
+        onView(withId(R.id.email)).check(matches(isDisplayed()));
+        onView(withId(R.id.email)).perform(typeText("Andrew"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withId(R.id.unknownTitleButton)).perform(click());
+        onView(withId(R.string.puzzles_completed)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkButtonLeaderboard(){
+        onView(withId(R.id.email)).check(matches(isDisplayed()));
+        onView(withId(R.id.email)).perform(typeText("Andrew"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withId(R.id.leaderboardButton)).perform(click());
+        onView(withId(R.id.classicButtonLeaderboard)).check(matches(isDisplayed()));
+        onView(withId(R.id.timeTrialLeaderboard)).check(matches(isDisplayed()));
+        onView(withId(R.id.arcadeButtonLeaderboard)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkButtonSettings(){
+        onView(withId(R.id.email)).check(matches(isDisplayed()));
+        onView(withId(R.id.email)).perform(typeText("Andrew"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withId(R.id.settingsButton)).perform(click());
+        onView(withText(R.string.description_color_tiles)).check(matches(isDisplayed()));
+    }
+
+
+    @After
+    public void finish(){
+
+    }
+}
